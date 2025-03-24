a8b8145ecb007547edb9bf4e0b5d555db90948bb
Elliott5000 <bartonel@students.dsbn.org>
2025-03-24 12:44:21 -0400

    Yippie

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+# Name: E. Barton
+# Title: lesson 16
+# Date: March 24, 2025
+# Description: This program finds the average of 100
+
+import random
+
+
+position = 0
+
+def sum (numbers):
+    add = 0
+    for i in range (100):
+        add = add + numbers[i]
+    result = add
+    return result
+
+def average (numbers):
+    result = sum(numbers) / 100
+    return result
+
+numbers = []
+for i in range (100):
+    num = random.randint(1,100)
+    numbers.append(num)
+
+print(numbers[1:99])
+print("Therefore the average of all numbers is " ,average(numbers))
 
